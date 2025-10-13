6cfc2ad292c4e109ce0648c648e4f711acbd223a
Maksim Efremov <hungryas@gmail.com>
2025-10-13 19:28:02 +0300

    feat: configure @DisableByIssue

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/rest/RegistrationTest.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/rest/RegistrationTest.java
@@ -1,8 +1,8 @@
 package guru.qa.niffler.test.rest;
 
+import guru.qa.niffler.jupiter.annotation.DisabledByIssue;
 import guru.qa.niffler.service.AuthApiClient;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import retrofit2.Response;
 
@@ -13,7 +13,7 @@ public class RegistrationTest {
   private final AuthApiClient authApiClient = new AuthApiClient();
 
   @Test
-  @Disabled
+  @DisabledByIssue("2")
   void newUserShouldRegisteredByApiCall() throws IOException {
     final Response<Void> response = authApiClient.register("bazz", "12345");
     Assertions.assertEquals(201, response.code());
