a54e1df8cf93e7556448bfc65452350cd0bd4c42
Maksim Efremov <42250205+mmefremov@users.noreply.github.com>
2026-04-04 11:04:08 +0300

    Homework 16.2 (#23)

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/api/UserdataSoapApi.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/api/UserdataSoapApi.java
@@ -1,7 +1,13 @@
 package guru.qa.niffler.api;
 
+import guru.qa.jaxb.userdata.AcceptInvitationRequest;
 import guru.qa.jaxb.userdata.AllUsersRequest;
 import guru.qa.jaxb.userdata.CurrentUserRequest;
+import guru.qa.jaxb.userdata.DeclineInvitationRequest;
+import guru.qa.jaxb.userdata.FriendsPageRequest;
+import guru.qa.jaxb.userdata.FriendsRequest;
+import guru.qa.jaxb.userdata.RemoveFriendRequest;
+import guru.qa.jaxb.userdata.SendInvitationRequest;
 import guru.qa.jaxb.userdata.UserResponse;
 import guru.qa.jaxb.userdata.UsersResponse;
 import retrofit2.Call;
@@ -24,4 +30,46 @@ public interface UserdataSoapApi {
     })
     @POST("ws")
     Call<UsersResponse> allUsers(@Body AllUsersRequest allUsersRequest);
+
+    @Headers({
+            "Content-type: text/xml",
+            "Accept-Charset: utf-8"
+    })
+    @POST("ws")
+    Call<UserResponse> sendInvitation(@Body SendInvitationRequest request);
+
+    @Headers({
+            "Content-type: text/xml",
+            "Accept-Charset: utf-8"
+    })
+    @POST("ws")
+    Call<UserResponse> acceptInvitation(@Body AcceptInvitationRequest request);
+
+    @Headers({
+            "Content-type: text/xml",
+            "Accept-Charset: utf-8"
+    })
+    @POST("ws")
+    Call<UserResponse> declineInvitation(@Body DeclineInvitationRequest request);
+
+    @Headers({
+            "Content-type: text/xml",
+            "Accept-Charset: utf-8"
+    })
+    @POST("ws")
+    Call<Void> removeFriend(@Body RemoveFriendRequest request);
+
+    @Headers({
+            "Content-type: text/xml",
+            "Accept-Charset: utf-8"
+    })
+    @POST("ws")
+    Call<UsersResponse> friends(@Body FriendsRequest request);
+
+    @Headers({
+            "Content-type: text/xml",
+            "Accept-Charset: utf-8"
+    })
+    @POST("ws")
+    Call<UsersResponse> friendsPage(@Body FriendsPageRequest request);
 }
--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/soap/SoapUsersTest.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/soap/SoapUsersTest.java
@@ -1,15 +1,16 @@
 package guru.qa.niffler.test.soap;
 
-import guru.qa.jaxb.userdata.CurrentUserRequest;
-import guru.qa.jaxb.userdata.UserResponse;
+import guru.qa.jaxb.userdata.FriendshipStatus;
 import guru.qa.niffler.jupiter.annotation.User;
 import guru.qa.niffler.jupiter.annotation.meta.SoapTest;
 import guru.qa.niffler.model.UserJson;
 import guru.qa.niffler.service.impl.UserdataSoapClient;
-import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SoapTest
 public class SoapUsersTest {
@@ -17,14 +18,63 @@ public class SoapUsersTest {
     private final UserdataSoapClient userdataSoapClient = new UserdataSoapClient();
 
     @Test
-    @User
-    void currentUserTest(UserJson user) throws IOException {
-        CurrentUserRequest request = new CurrentUserRequest();
-        request.setUsername(user.username());
-        UserResponse response = userdataSoapClient.currentUser(request);
-        Assertions.assertEquals(
-                user.username(),
-                response.getUser().getUsername()
-        );
+    @DisplayName("Список друзей получен в виде Page при передаче параметров page, size")
+    @User(friends = 3)
+    void friendListShouldBePageable(UserJson user) throws IOException {
+        var response = userdataSoapClient.friends(user.username(), 0, 3);
+        assertThat(response.getTotalElements()).isEqualTo(3);
+        assertThat(response.getUser()).hasSize(1)
+                .extracting(guru.qa.jaxb.userdata.User::getFriendshipStatus)
+                .allMatch(status -> status == FriendshipStatus.FRIEND);
+    }
+
+    @Test
+    @DisplayName("Список друзей с фильтраций по username")
+    @User(friends = 3)
+    void friendListFilteredBySearchQuery(UserJson user) throws IOException {
+        String friendName = user.testData().friends().getFirst().username();
+        var response = userdataSoapClient.friends(user.username(), friendName);
+        assertThat(response.getTotalElements()).isOne();
+        assertThat(response.getUser()).hasSize(1)
+                .first().matches(
+                friend -> friend.getUsername().equals(friendName) && friend.getFriendshipStatus() == FriendshipStatus.FRIEND);
+    }
+
+    @Test
+    @DisplayName("Дружба должна удаляться")
+    @User(friends = 1)
+    void removeFriendship(UserJson user) throws IOException {
+        String friendName = user.testData().friends().getFirst().username();
+        userdataSoapClient.removeFriend(user.username(), friendName);
+        var response = userdataSoapClient.friends(user.username());
+        assertThat(response.getUser()).isEmpty();
+    }
+
+    @Test
+    @DisplayName("Прием заявки в друзья")
+    @User(incomeInvitations = 1)
+    void acceptIncomeInvitation(UserJson user) throws IOException {
+        String addresseeName = user.testData().incomeInvitations().getFirst().username();
+        var response = userdataSoapClient.acceptInvitation(user.username(), addresseeName);
+        assertThat(response.getUser().getFriendshipStatus()).isEqualTo(FriendshipStatus.FRIEND);
+    }
+
+    @Test
+    @DisplayName("Отклонение заявки в друзья")
+    @User(incomeInvitations = 1)
+    void rejectIncomeInvitation(UserJson user) throws IOException {
+        String addresseeName = user.testData().incomeInvitations().getFirst().username();
+        var response = userdataSoapClient.declineInvitation(user.username(), addresseeName);
+        assertThat(response.getUser().getFriendshipStatus()).isEqualTo(FriendshipStatus.VOID);
+    }
+
+    @Test
+    @DisplayName("Отправка приглашения дружить")
+    @User(incomeInvitations = 1)
+    void sendFriendshipInvitation(UserJson user) throws IOException {
+        String addresseeName = user.testData().incomeInvitations().getFirst().username();
+        userdataSoapClient.declineInvitation(user.username(), addresseeName);
+        var response = userdataSoapClient.sendInvitation(user.username(), addresseeName);
+        assertThat(response.getUser().getFriendshipStatus()).isEqualTo(FriendshipStatus.INVITE_SENT);
     }
 }
